2ae29a473c7c46c714e63c3f1c346ef94964c485
duangsuse <fedora-opensuse@outlook.com>
2019-10-28 00:49:48 +0800

    Refine(Struct): seq structure I/O class: vararg

--- src/main/kotlin/org/duangsuse/dokuss/struct/Seq.kt
+++ src/main/kotlin/org/duangsuse/dokuss/struct/Seq.kt
@@ -8,7 +8,9 @@ import org.duangsuse.dokuss.intf.Writer
  * Takes a function returns [ArrayLike] object allocation,
  *  parse child struct, store them.
  */
-class Seq<T: ArrayLike>(private val st: () -> T, private val children: Iterable<Struct<T>>): Struct<T> {
+class Seq<T: ArrayLike, E>(private val st: () -> T, private val children: Iterable<Struct<E>>): Struct<T> {
+  constructor(st: () -> T, vararg children: Struct<E>): this(st, children.asIterable())
+
   override fun read(r: Reader): T {
     val struct = st()
     for (child in children.withIndex())
@@ -20,7 +22,7 @@ class Seq<T: ArrayLike>(private val st: () -> T, private val children: Iterable<
     val writers = children.map { return@map it::write }
     for (xi in 0 until data.dims) {
       @Suppress("UNCHECKED_CAST")
-      writers[xi](w, data[xi] as T)
+      writers[xi](w, data[xi] as E)
     }
   }
 
